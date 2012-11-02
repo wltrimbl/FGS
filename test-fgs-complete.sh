161f91a4c04ee9b3d47e186dcb8df0ca02f7e2a9
Will Trimble <trimble@anl.gov>
2012-11-02 15:37:56 -0500

    wrong path assumption in test

--- test-fgs-complete.sh
+++ test-fgs-complete.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -e $1 ] 
 then
-./run_FragGeneScan.pl -genome=$1     -out=$2  -complete=1  -train=complete
+run_FragGeneScan.pl -genome=$1     -out=$2  -complete=1  -train=complete
 else 
 echo "Usage : test-fgs-complete.sh <input.fasta> <outstem>"
 fi
