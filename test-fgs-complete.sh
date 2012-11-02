4911ec4296dc3b18eb22663ad8ed52f77b7fa7e4
Will Trimble <trimble@anl.gov>
2012-11-02 15:30:31 -0500

    removed .o files, added simple test

---
+++ test-fgs-complete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -e $1 ] 
+then
+./run_FragGeneScan.pl -genome=$1     -out=$2  -complete=1  -train=complete
+else 
+echo "Usage : test-fgs-complete.sh <input.fasta> <outstem>"
+fi
+
+
