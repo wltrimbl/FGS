64a028079d9fedc3c30ad46051b4d4916140b3a4
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2012-11-08 10:56:48 -0600

    added stdin parser

--- run_hmm.c
+++ run_hmm.c
@@ -11,8 +11,9 @@
 int main (int argc, char **argv){
 
 	int i, j, c, num_seq;
-	HMM hmm;
-	char *obs_seq, *obs_head;
+	HMM my_hmm;
+	char *obs_seq = 0;
+	char *obs_head = 0;
 	TRAIN train;
 	int wholegenome=-1;
 	int format=0;
@@ -28,14 +29,19 @@ int main (int argc, char **argv){
 	char nstate_file[4096] = "";
 	char sstate_file[4096] = "";
 	char pstate_file[4096] = "";
-	char s1state_file[4096] = "";     /* stop codon of gene in - stand */
+	char s1state_file[4096] = "";		 /* stop codon of gene in - stand */
 	char p1state_file[4096] = "";
 	char dstate_file[4096] = "";
 	char train_dir[4096] = "";
 	int count=0;
-	char mystring[1000] = "";        /* input buffer  */
-	int *obs_seq_len;
-	int bp_count=0;  /* count the length of each line in input file */
+	char mystring[1000] = "";				/* input buffer	*/
+	int mylongbuffer_len = 5000000;
+	//char mylongbuffer[5000000] = "";
+	char * mylongbuffer = 0;
+	int data_len;
+		
+	int *obs_seq_len = 0;
+	int bp_count=0;	/* count the length of each line in input file */
 
 	strncpy(train_dir, argv[0], strlen(argv[0])-12);
 	strcat(train_dir, "train/");
@@ -58,7 +64,7 @@ int main (int argc, char **argv){
 
 
 	/* read command line argument */
-	if (argc <= 8){    
+	if (argc <= 8){		
 		fprintf(stderr, "ERROR: You missed some parameters for input\n");
 		print_usage();
 		exit(EXIT_FAILURE);
@@ -69,12 +75,13 @@ int main (int argc, char **argv){
 		switch (c){
 		case 's':
 			strcpy(seq_file, optarg);
+			if (strcmp(seq_file, "stdin") != 0 ) {	
 				if (access(seq_file, F_OK)==-1){
 					fprintf(stderr, "ERROR: Sequence file [%s] does not exist\n", seq_file);
 					print_usage();
 					exit(EXIT_FAILURE);
-
 				}
+			}
 			break;	
 		case 'w':
 			wholegenome = atoi(optarg);
@@ -148,8 +155,8 @@ int main (int argc, char **argv){
 	}
 
 	/* read all initial model */
-	hmm.N=NUM_STATE;
-	get_train_from_file(hmm_file, &hmm, mstate_file, rstate_file, nstate_file, sstate_file, pstate_file,s1state_file, p1state_file, dstate_file, &train);
+	my_hmm.N=NUM_STATE;
+	get_train_from_file(hmm_file, &my_hmm, mstate_file, rstate_file, nstate_file, sstate_file, pstate_file,s1state_file, p1state_file, dstate_file, &train);
  
 	/* create output file name */
 	strcpy(aa_file, out_file);
@@ -168,11 +175,85 @@ int main (int argc, char **argv){
 	fp_dna = fopen (dna_file , "w");
 	if(fp_dna ==0) {fprintf(stderr, "error opening dna file %s\n", dna_file); exit(EXIT_FAILURE);}
 
- /*  Parse input, count records, store in int num_seq */
-	fp = fopen (seq_file, "r");
-	fgets(mystring, 2, fp);
-	if ( ( mystring[0] != '>')) {fprintf(stderr, "Input data '%s' not in fasta format \n", seq_file); exit(EXIT_FAILURE);}
-	rewind(fp);
+	
+	if (strcmp(seq_file, "stdin") == 0 ) {
+		fp = stdin;
+		
+		mylongbuffer = (char *)malloc(mylongbuffer_len);
+		
+		obs_seq = (char *)malloc(mylongbuffer_len);
+		obs_seq[0] = '\0';
+		while ( fgets (mylongbuffer , mylongbuffer_len , fp) ){
+			//need to defined obs_seq and obs_head
+			data_len = strlen(mylongbuffer);
+			if ( data_len >= mylongbuffer_len -1) {
+				fprintf(stderr, "error: buffer full, use stdin only for short sequences \n");
+				exit(EXIT_FAILURE);
+			}
+			//fprintf(stdout, "reading: %s\n", mylongbuffer);
+			while (mylongbuffer[data_len-1] == 10 || mylongbuffer[data_len-1] == 13 ) {
+				mylongbuffer[data_len-1] = '\0';
+				data_len--;
+				if (data_len == '\0') {
+					fprintf(stderr, "error: header or sequence too short\n");
+					exit(EXIT_FAILURE);
+				}
+			}
+			
+			if (mylongbuffer[0] == '>'){
+				//count++;
+				
+				if (obs_seq[0] != '\0') {
+					// process previous sequence
+					
+					//fprintf(stdout, "give: \"%s\"\n", obs_head);
+					//fprintf(stdout, "give: \"%s\"\n", obs_seq);
+					
+					get_prob_from_cg(&my_hmm, &train, obs_seq);
+					
+					if (strlen(obs_seq)>70){
+						viterbi(&my_hmm, obs_seq, fp_out, fp_aa, fp_dna, obs_head, wholegenome, format);
+					}
+					
+					
+					free(obs_head);
+					obs_head = 0;
+					
+					obs_seq[0] = '\0';
+				}
+				
+				
+				obs_head = (char *)malloc(data_len+1);
+				memcpy(obs_head, mylongbuffer, data_len);
+				obs_head[data_len] = '\0';
+			} else {
+				strcat(obs_seq, mylongbuffer);
+			}
+			
+			
+		}
+		// last sequence
+		if (obs_seq != 0) {
+			
+			get_prob_from_cg(&my_hmm, &train, obs_seq);
+			
+			if (strlen(obs_seq)>70){
+				viterbi(&my_hmm, obs_seq, fp_out, fp_aa, fp_dna, obs_head, wholegenome, format);
+			}
+			
+			
+			free(obs_head);
+			obs_head = 0;
+			
+			free(obs_seq);
+			obs_seq = 0;
+		}
+		
+		
+	} else {
+	
+	 /*	Parse input, count records, store in int num_seq */
+		fp = fopen (seq_file, "r");
 		while ( fgets (mystring , sizeof mystring , fp) ){
 			if (mystring[0] == '>'){
 				count++;
@@ -181,12 +262,12 @@ int main (int argc, char **argv){
 		num_seq = count;
 		obs_seq_len = (int *)malloc(num_seq * sizeof(int));
 				if (!obs_seq_len) {
-					fprintf(stderr, "%s\n", "ERROR: Allocation failure for obs_seq_len");
-					exit(EXIT_FAILURE);
-					}
+					 fprintf(stderr, "%s\n", "ERROR: Allocation failure for obs_seq_len");
+					 exit(EXIT_FAILURE);
+					 }
 		printf("no. of seqs: %d\n", num_seq);
 
-		/*  Parse input, count record length, store in obs_seq_len */
+		/*	Parse input, count record length, store in obs_seq_len */
 		i = 0;
 		count = 0;
 		rewind(fp);
@@ -208,49 +289,49 @@ int main (int argc, char **argv){
 		}
 		obs_seq_len[count] = i;
 
-	/*	Parse input, read data, store in obs_head and obs_seq fields and call viterbi  */
+	/*	Parse input, read data, store in obs_head and obs_seq fields and call viterbi	*/
 		count=-1;
 		rewind(fp);
 		j = 0;
 		obs_head = (char *)malloc(sizeof(mystring));
 				if (!obs_head) {
-					fprintf(stderr, "%s\n", "ERROR: Allocation failure for obs_head");
-					exit(EXIT_FAILURE);
-					}
+					 fprintf(stderr, "%s\n", "ERROR: Allocation failure for obs_head");
+					 exit(EXIT_FAILURE);
+					 }
 		memset(obs_head, 0, (bp_count+1) * sizeof(char));
 		obs_seq = (char *)malloc(obs_seq_len[0] * sizeof(char) + 1);
 				if (!obs_seq) {
-					fprintf(stderr, "%s\n", "ERROR: Allocation failure for obs_seq");
-					exit(EXIT_FAILURE);
-					}
+					 fprintf(stderr, "%s\n", "ERROR: Allocation failure for obs_seq");
+					 exit(EXIT_FAILURE);
+					 }
 
-		while ( fgets (mystring , sizeof mystring  , fp) ){
+		while ( fgets (mystring , sizeof mystring	, fp) ){
 
 			if (mystring[0] == '>'){
-					if(strlen(mystring) == (sizeof mystring) -1) {
-						 fprintf(stderr, "ERROR: FASTA header too long!  %d bytes\n", (int)strlen(mystring));
-						exit(EXIT_FAILURE);
-						}
-				if (count>=0 && j>0){  // process previous sequence
+					 if(strlen(mystring) == (sizeof mystring) -1) {
+						 fprintf(stderr, "ERROR: FASTA header too long!	%d bytes\n", (int)strlen(mystring));
+						 exit(EXIT_FAILURE);
+						 }
+				if (count>=0 && j>0){	// process previous sequence
 
-		get_prob_from_cg(&hmm, &train, obs_seq);
+		get_prob_from_cg(&my_hmm, &train, obs_seq);
 
 		if (strlen(obs_seq)>70){
-			viterbi(&hmm, obs_seq, fp_out, fp_aa, fp_dna, obs_head, wholegenome, format);
+			viterbi(&my_hmm, obs_seq, fp_out, fp_aa, fp_dna, obs_head, wholegenome, format);
 		}
 				}
 
-				bp_count = strlen(mystring)-1;   // handle fasta header 
+				bp_count = strlen(mystring)-1;	 // handle fasta header 
 				while(mystring[bp_count-1] == 10 || mystring[bp_count-1]==13){
 		bp_count --;
 				}
 				memset(obs_head, 0, (bp_count+1) * sizeof(char));
 				memcpy(obs_head, mystring, bp_count);
 
-				if (count == -1 || (count>=0 && j>0) ){   // allocate and initially populate obs_seq
+				if (count == -1 || (count>=0 && j>0) ){	 // allocate and initially populate obs_seq
 		count++;
 		obs_seq = (char *)malloc(obs_seq_len[count] * sizeof(char) + 1);
-				if (!obs_seq) {
+				 if (!obs_seq) {
 						fprintf(stderr, "%s\n", "ERROR: Allocation failure for obs_seq");
 						exit(EXIT_FAILURE);
 						}
@@ -258,7 +339,7 @@ int main (int argc, char **argv){
 				}
 				j = 0;
 
-			}else{                                     // append to obs_seq
+			}else{																		 // append to obs_seq
 				bp_count = strlen(mystring)-1;
 				while(mystring[bp_count-1] == 10 || mystring[bp_count-1]==13){
 		bp_count --;
@@ -268,16 +349,17 @@ int main (int argc, char **argv){
 			}
 		}
 
-		if (count>=0){                              // take care of final sequence
+		if (count>=0){															// take care of final sequence
 
-			get_prob_from_cg(&hmm, &train, obs_seq);
+			get_prob_from_cg(&my_hmm, &train, obs_seq);
 
 			if (strlen(obs_seq)>70){
-				viterbi(&hmm, obs_seq, fp_out, fp_aa, fp_dna, obs_head, wholegenome, format);
+				viterbi(&my_hmm, obs_seq, fp_out, fp_aa, fp_dna, obs_head, wholegenome, format);
 			}
 		}
-
-
+	}
+	
+	
 	free(obs_seq_len);
 	free(obs_head);
 	free(obs_seq);
